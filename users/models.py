0596117f57ebf8466ed370bd4f4b9490af5fc023
Michael-Otieno <michael.otieno@student.moringaschool.com>
2022-10-10 01:41:03 +0300

    create users model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+  first_name = models.CharField(max_length=255)
+  last_name = models.CharField(max_length=255)
+  email = models.EmailField(max_length=255)
+  remotask_id = models.CharField(max_length=255)
+  password = models.CharField(max_length=255)
+  # confirm_password = serializers.CharField(max_length=255, min_length=8, write_only=True)
+  class Meta:
+        ordering = ['email']
