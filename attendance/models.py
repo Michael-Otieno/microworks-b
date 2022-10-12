e0795598443df997678e55e5743019d28e363df0
Michael-Otieno <michael.otieno@student.moringaschool.com>
2022-10-12 06:45:50 +0300

    add attendance form

--- attendance/models.py
+++ attendance/models.py
@@ -1,3 +1,15 @@
+import email
 from django.db import models
 
 # Create your models here.
+
+class Attendance(models.Model):
+  full_names = models.CharField(max_length=255)
+  email = models.EmailField(max_length=255)
+  machine_id = models.CharField(max_length=255)
+  availability = models.TextField()
+  date = models.DateField(null=False, blank=False)
+  time_in = models.DateTimeField(auto_now_add=True)
+
+  class Meta:
+    get_latest_by='time_in'
