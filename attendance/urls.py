66818c367f344d51e25328e8da36b630bb8cb169
Michael-Otieno <michael.otieno@student.moringaschool.com>
2022-10-15 05:11:22 +0300

    add swagger url

---
+++ attendance/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from . import views
+
+urlpatterns=[
+  path('attendance-list/',views.AttendanceList.as_view()),
+  path('attendance/<int:pk>/',views.AttendanceDetail.as_view()),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
