e55ef5c236578645457d7785f889f6d7996d9457
Michael-Otieno <michael.otieno@student.moringaschool.com>
2022-10-15 05:12:14 +0300

    add attendance serializer

---
+++ attendance/serializers.py
@@ -0,0 +1,11 @@
+from dataclasses import field
+from pyexpat import model
+from rest_framework import serializers
+from .models import Attendance
+
+
+class AttendanceSerializer(serializers.ModelSerializer):
+
+  class Meta:
+    model=Attendance
+    fields=['id','full_names','email','machine_id','availability','date','time_in']
